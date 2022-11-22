6a3f7faf12ab4384a2a793dc3e3cf556dbdae1cb
SammyLeths <s.o.eyiowuawi@gmail.com>
2022-11-22 19:11:06 +0100

    implemented constant velocity verical slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -93,48 +93,6 @@ allSections.forEach(function (section) {
 });
 
 //////////////////////////////////////////////////
-// GALLERY DRAG AND SCROLL
-
-const scroller = document.querySelector('.gallery__wrapper__images');
-const imageBox = document.querySelectorAll('.gallery__wrapper__images__box');
-
-let isGrabbed = false;
-let initialPos;
-let scrollTop;
-
-const initializeDrag = (e) => {
-  scroller.classList.add('active');
-  imageBox.forEach((box) => {
-    box.classList.add('shadow');
-  });
-
-  isGrabbed = true;
-  initialPos = e.pageY - scroller.offsetTop;
-  scrollTop = scroller.scrollTop;
-};
-
-const handleDragging = (e) => {
-  if (!isGrabbed) return;
-  e.preventDefault();
-  const yPos = e.pageY - scroller.offsetTop;
-  const walk = (yPos - initialPos) * 2;
-  scroller.scrollTop = scrollTop - walk;
-};
-
-const deInitializeDrag = () => {
-  isGrabbed = false;
-  scroller.classList.remove('active');
-  imageBox.forEach((box) => {
-    box.classList.remove('shadow');
-  });
-};
-
-scroller.addEventListener('mousedown', initializeDrag);
-scroller.addEventListener('mousemove', handleDragging);
-scroller.addEventListener('mouseleave', deInitializeDrag);
-scroller.addEventListener('mouseup', deInitializeDrag);
-
-//////////////////////////////////////////////////
 // THEME SWITCHERS
 
 const body = document.body;
